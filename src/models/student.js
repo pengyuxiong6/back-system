32290569695973848de6d111e24cfcf9f891bd7b
peng <876341347@qq.com>
2019-07-18 22:16:45 +0800

    '完成删除跟搜索'

--- src/models/student.js
+++ src/models/student.js
@@ -2,6 +2,7 @@ export default {
   namespace: 'student',
   state: {
     list: [], //学生数据
+    searchName: "",//搜索关键字
     limit: 5,//每页显示的条数
     page:1,//当前页数
     total: 1,//数据的总条数
@@ -9,9 +10,14 @@ export default {
 
   effects: {
     *getList(action,{ put, select }) {
-        let page = action.page;
-        let limit = action.pageSize;
-        let url = `http://localhost:3000/student?_page=${page}&_limit=${limit}`;
+        let page = yield select(state => state.student.page);
+        let limit = yield select(state => state.student.limit);
+        let searchName = yield select(state => state.student.searchName);
+
+        page = action.page || page;
+        limit = action.pageSize || limit;
+        searchName = action.searchName || searchName;
+        let url = `http://localhost:3000/student?_page=${page}&_limit=${limit}&name_like=${searchName}`;
         let response = yield fetch(url);
         let res = yield response.json();
         let total = yield parseInt(response.headers.get('X-Total-Count')) ;
@@ -20,8 +26,23 @@ export default {
             list: res ,
             total,
             page,
-            limit
+            limit,
+            searchName
             })
+    },
+    //删除某个学生
+    *delStu(action,{ put }) {
+        //1. 学生id
+        let id = action.id;
+        //2. 请求
+        let response = yield fetch(`http://localhost:3000/student/${id}`,{
+            method: "DELETE",
+        });
+        let res = yield response.json();
+        //3.1 直接操作仓库
+        // yield put({ type: "delStusync", id });
+        //3.2 可以让 getList 重新执行
+        yield put({ type: "getList" })
     }
   },
 
@@ -32,8 +53,19 @@ export default {
         list: action.list,
         total: action.total,
         page: action.page,
-        limit: action.limit
+        limit: action.limit,
+        searchName: action.searchName
       });
     },
+    delStusync(state,{ id }) {
+        //将list 中的某一项给删除
+        //得到要删除的学生在学生集合中的下标
+        let index = state.list.findIndex(item => item.id === id);
+        let newList = [...state.list];
+        newList.splice(index,1);
+        return Object.assign({},state, {
+            list: newList
+        })
+    }
   },
 };
--- src/pages/student/index.js
+++ src/pages/student/index.js
@@ -1,71 +1,123 @@
 import styles from './index.less';
-import { Table, Button } from 'antd';
+import { Table, Button, Input, Popconfirm, Modal,Form } from 'antd';
 import { connect } from 'dva';
-import React from "react";
-
-//表格列的数据表现
-const columns = [
-  {
-    title: '序号', //列名
-    dataIndex: 'id', //用数据的那个来渲染这个列
-  },
-  {
-    title: '名字',
-    dataIndex: 'name',
-  },
-  {
-    title: '年龄',
-    dataIndex: 'age',
-  },
-  {
-    title: '性别',
-    dataIndex: 'sex',
-    //自定义显示模板
-    render: (text, row, index) => {
-      //text  当前行的指定属性的数据
-      //row   当前行的数据 { id: 1,name: "",age, sex }
-      return <div>{text === 1 ? '男' : '女'}</div>;
-    },
-  },
-  {
-    title: '操作',
-    render: (text, row, index) => {
-      return (
-        <div>
-          <Button>修改</Button>
-          <Button
-            onClick={() => {
-              alert('当前的id是' + row.id);
-            }}
-          >
-            删除
-          </Button>
-        </div>
-      );
-    },
-  },
-];
+import React from 'react';
 
 class Student extends React.Component {
+state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  //表格列的数据表现
+  columns = [
+    {
+      title: '序号', //列名
+      dataIndex: 'id', //用数据的那个来渲染这个列
+    },
+    {
+      title: '名字',
+      dataIndex: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+    },
+    {
+      title: '性别',
+      dataIndex: 'sex',
+      //自定义显示模板
+      render: (text, row, index) => {
+        //text  当前行的指定属性的数据
+        //row   当前行的数据 { id: 1,name: "",age, sex }
+        return <div>{text === 1 ? '男' : '女'}</div>;
+      },
+    },
+    {
+      title: '操作',
+      render: (text, row, index) => {
+        return (
+          <div>
+            <Button onClick={this.showModal}>修改</Button>
+            <Popconfirm
+              title="你真的要删除哇"
+              onCancel={() => {
+                console.log('取消了');
+              }}
+              onConfirm={() => {
+                this.props.delStu(row.id);
+              }}
+            >
+              <Button>删除</Button>
+            </Popconfirm>
+          </div>
+        );
+      },
+    },
+  ];
+  
   render() {
     return (
       <div>
-        <Table dataSource={this.props.data} columns={columns}
-        pagination= {{
-            total: this.props.total,//数据总条数
-            defaultPageSize:this.props.limit,//默认每页显示多少条
+        {/* 1. 字符串的形式，this.refs.myInput
+          2. 函数的形式， this.myInput
+          <Input ref="myInput"/> */}
+        <Input
+          className={styles.inp}
+          ref={el => {
+            this.myInput = el;
+          }}
+        />
+        <Button
+          onClick={() => {
+            console.log(this.myInput.state.value);
+            this.props.getList(1, this.props.limit, this.myInput.state.value);
+          }}
+        >
+          搜索
+        </Button>
+        <Table
+          dataSource={this.props.data}
+          columns={this.columns}
+          pagination={{
+            total: this.props.total, //数据总条数
+            defaultPageSize: this.props.limit, //默认每页显示多少条
             onChange: (page, pageSize) => {
-                console.log(page,pageSize);
-                this.props.getList(page, pageSize);
-            }
-        }}
-        rowKey="id" />
+              this.props.getList(page, pageSize);
+            },
+          }}
+          rowKey="id"
+        />
+        <Modal
+          title="学生信息"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        > 
+        </Modal>
       </div>
     );
   }
 
   componentDidMount() {
-      this.props.getList();
+    this.props.getList();
   }
 }
 
@@ -75,18 +127,26 @@ export default connect(
       data: state.student.list,
       limit: state.student.limit,
       total: state.student.total,
+      page: state.student.page,
     };
   },
   dispatch => {
     return {
-        //获取学生列表
-        //page 请求的页码
-        //pageSize 请求每页的条数
-      getList: (page = 1, pageSize = 5) => {
-        dispatch({ 
-            type: 'student/getList',
-            page,
-            pageSize
+      //获取学生列表
+      //page 请求的页码
+      //pageSize 请求每页的条数
+      getList: (page = 1, pageSize = 5, searchName = '') => {
+        dispatch({
+          type: 'student/getList',
+          page,
+          pageSize,
+          searchName,
+        });
+      },
+      delStu: id => {
+        dispatch({
+          type: 'student/delStu',
+          id,
         });
       },
     };
